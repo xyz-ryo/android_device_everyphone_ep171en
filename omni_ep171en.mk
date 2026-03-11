bb60558d08121cd84337e7ad86ad41b7e52edb8a
xyz-ryo <kingsizebox2222@gmail.com>
2026-03-11 22:10:15 +0900

    Create omni_ep171en.mk

---
+++ omni_ep171en.mk
@@ -0,0 +1,9 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+PRODUCT_DEVICE := ep171en
+PRODUCT_NAME := omni_ep171en
+PRODUCT_BRAND := EveryPhone
+PRODUCT_MODEL := EP-171EN
+PRODUCT_MANUFACTURER := everyphone
+
+PRODUCT_GMS_CLIENTID_BASE := android-everyphone
